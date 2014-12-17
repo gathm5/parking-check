1cf1195a2e8927b7d7b3557c3511b8c1fd3ea1e5
Gautham Stalin <Gautham.Stalin@monitise.com>
2014-12-16 16:57:23 -0800

    mapquest

---
+++ app/scripts/services/transit-service-api.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('parkingCheckApp')
+    .constant('$transitConfig', {
+        example: [
+            'http://www.mapquestapi.com/directions/v2/route?key=Fmjtd%7Cluurn1uz2u%2Cbl%3Do5-9wyx50&from=37.973591,-122.532823&to=37.973489,-122.532030&routeType=pedestrian',
+            'key=Fmjtd%7Cluurn1uz2u%2Cbl%3Do5-9wyx50',
+            'callback=renderAdvancedNarrative',
+            'ambiguities=ignore',
+            'avoidTimedConditions=false',
+            'doReverseGeocode=true',
+            'outFormat=json',
+            'routeType=fastest',
+            'timeType=1',
+            'enhancedNarrative=false',
+            'shapeFormat=raw',
+            'generalize=0',
+            'locale=en_US',
+            'unit=m',
+            'from=Clarendon%20Blvd,%20Arlington,%20VA',
+            'to=2400%20S%20Glebe%20Rd,%20Arlington,%20VA',
+            'drivingStyle=2',
+            'highwayEfficiency=21.0'
+        ],
+        url: 'http://www.mapquestapi.com/directions/v2/route',
+        configuration: {
+            key: 'Fmjtd%7Cluurn1uz2u%2Cbl%3Do5-9wyx50',
+            ambiguities: 'ignore',
+            routeType: 'pedestrian',
+            from: 'Clarendon%20Blvd,%20Arlington,%20VA',
+            to: '2400%20S%20Glebe%20Rd,%20Arlington,%20VA'
+        }
+    })
+    .service('$transit', [
+        '$q',
+        '$http',
+        '$transitConfig',
+        function TransitServiceApi($q, $http, $transitConfig) {
+            // AngularJS will instantiate a singleton by calling 'new' on this function
+            var serviceUrl = $transitConfig.url;
+            var params = $transitConfig.configuration;
+
+            function get(fromGeo, toGeo) {
+                params.from = fromGeo.latitude + ',' + fromGeo.longitude;
+                params.to = toGeo.latitude + ',' + toGeo.longitude;
+                $http.get(serviceUrl, {
+                    params: params
+                })
+            }
+        }
+    ]);
