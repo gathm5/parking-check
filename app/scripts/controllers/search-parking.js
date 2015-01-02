51aaf90635b1d053d479e3be2ef5d61ca659e145
Gautham Stalin <Gautham.Stalin@monitise.com>
2015-01-02 11:57:53 -0800

    flag

--- app/scripts/controllers/search-parking.js
+++ app/scripts/controllers/search-parking.js
@@ -9,22 +9,29 @@ angular.module('parkingCheckApp')
         '$state',
         '$rootScope',
         function ($scope, $config, $http, $geocode, $state, $rootScope) {
+            $scope.customLocation = null;
+            $scope.searching = {};
+            $scope.failure = {};
+
             function call(destination) {
                 var url = $config.api.builder(destination);
                 $http
                     .get(url)
                     .then(function (output) {
                         $scope.parking = output.data;
+                        $scope.searching.started = false;
+                    }, function (reason) {
+                        $scope.failure.reason = reason;
+                        $scope.searching.started = false;
                     });
             }
-
-            $scope.customLocation = null;
 
             $scope.findNearMe = function () {
                 $rootScope.$emit('$alert', {
                     message: 'Searching for parking near your current location',
                     showTime: 5 * 1000
                 });
+                $scope.searching.started = true;
                 $geocode
                     .geocode($scope)
                     .then(function (location) {
@@ -36,15 +43,18 @@ angular.module('parkingCheckApp')
                             message: reject,
                             showTime: 3 * 1000
                         });
+                        $scope.searching.started = false;
                     }, function (update) {
                         $rootScope.$emit('$alert', {
                             message: update,
                             showTime: 3 * 1000
                         });
+                        $scope.searching.started = false;
                     });
             };
 
             $scope.findParking = function () {
+                $scope.searching.started = true;
                 $rootScope.$emit('$alert', {
                     message: 'Searching for parking places',
                     showTime: 5 * 1000
